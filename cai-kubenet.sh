da144748caed9883bb5654ec612f742fded95e60
Quang Tran Van <quangtv@nuce.edu.vn>
2021-03-04 17:09:04 +0700

    sua file cai dat

--- cai-kubenet.sh
+++ cai-kubenet.sh
@@ -1,4 +1,23 @@
+# Cai dat Docker
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum update -y && yum install docker-ce-18.06.2.ce -y
+usermod -aG docker $(whoami)
 
+## Create /etc/docker directory.
+mkdir /etc/docker
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
   ]
 }
 EOF
